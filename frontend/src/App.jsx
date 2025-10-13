96dad603297bbf8badd2f468699c41632c731a28
AiSharath <sharathbhat755@gmail.com>
2025-10-13 09:45:13 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
 import Register from "./Register.jsx"
 import Login from "./Login.jsx";
 import Home from "./Home.jsx";
-import registerUser from "./registerUser.jsx";
+import RegisterUser from "./registerUser.jsx";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/register" element={<registerUser/>}/>
+          <Route path="/register" element={<RegisterUser/>}/>
           <Route path="/report" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
         </Routes>
