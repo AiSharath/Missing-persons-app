544f3a36a4d271b7dcc1478bb67dc4107582b8be
AiSharath <sharathbhat755@gmail.com>
2025-10-27 18:27:03 +0530

    Made changes to user schema

--- backend/model/User.js
+++ backend/model/User.js
@@ -18,9 +18,25 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength: 6,
     },
+    faceDescriptor: {
+      type: [Number],  // Array of numbers (128-dimension face embeddings)
+      required: true,
+    },
   },
   { timestamps: true }
 );
+
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
